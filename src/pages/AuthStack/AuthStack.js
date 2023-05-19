c0da1af27ecc58036a7a5ddde749d44fd48d21d0
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2023-05-19 22:09:55 +0300

    FIX:Navigate bug fixed.

---
+++ src/pages/AuthStack/AuthStack.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import LoginRegister from '../LoginRegisterPage/LoginRegisterPage';
+
+const Tab = createMaterialTopTabNavigator();
+
+const AuthStack = () => {
+    return (
+      <Tab.Navigator screenOptions={{
+        swipeEnabled:false
+      }}>
+        <Tab.Screen name="Kayıt Ol">
+          {() => <LoginRegister type="register" />}
+        </Tab.Screen>
+        <Tab.Screen name="Giriş Yap">
+          {() => <LoginRegister type="login" />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    );
+};
+
+export default AuthStack;
